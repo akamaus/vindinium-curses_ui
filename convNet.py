35927181914e04a24a90b3495872c3ea5538162d
Jan Grzybowski <JanGrzybowski@outlook.com>
2016-06-06 23:30:00 +0200

    Saving the Convnet variables

--- convNet.py
+++ convNet.py
@@ -24,14 +24,14 @@ GAMMA = 0.6
 TRAINING_BATCH_SIZE = 100
 
 
-def weight_variable(shape):
+def weight_variable(shape, name):
     initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
+    return tf.Variable(initial, name=name)
 
 
-def bias_variable(shape):
+def bias_variable(shape, name):
     initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+    return tf.Variable(initial, name=name)
 
 
 def conv2d(x, W):
@@ -47,23 +47,23 @@ def _create_network():
     sess = tf.Session()
     input_layer = tf.placeholder(tf.float32, shape=[None, MAX_MAP_SIZE, MAX_MAP_SIZE, DATA_CHANNELS])
     # Layer 1
-    W_conv1 = weight_variable([LAYER1_PATCH_SIZE, LAYER1_PATCH_SIZE, DATA_CHANNELS, LAYER1_FEATURES])
-    b_conv1 = bias_variable([LAYER1_FEATURES])
+    W_conv1 = weight_variable([LAYER1_PATCH_SIZE, LAYER1_PATCH_SIZE, DATA_CHANNELS, LAYER1_FEATURES], name = "W_conv1")
+    b_conv1 = bias_variable([LAYER1_FEATURES], name = "b_conv1")
     h_conv1 = tf.nn.relu(conv2d(input_layer, W_conv1) + b_conv1)
     h_pool1 = max_pool(h_conv1)
     # Layer 2
-    W_conv2 = weight_variable([LAYER1_PATCH_SIZE, LAYER2_PATCH_SIZE, LAYER1_FEATURES, LAYER2_FEATURES])
-    b_conv2 = bias_variable([LAYER2_FEATURES])
+    W_conv2 = weight_variable([LAYER1_PATCH_SIZE, LAYER2_PATCH_SIZE, LAYER1_FEATURES, LAYER2_FEATURES], name = "W_conv2")
+    b_conv2 = bias_variable([LAYER2_FEATURES], name = "b_conv2")
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool(h_conv2)
     # Layer 3
-    W_fc1 = weight_variable([FINAL_VECTOR_SIZE, FINAL_FEATURES])
-    b_fc1 = bias_variable([FINAL_FEATURES])
+    W_fc1 = weight_variable([FINAL_VECTOR_SIZE, FINAL_FEATURES], name = "W_fc1")
+    b_fc1 = bias_variable([FINAL_FEATURES], name = "b_fc1")
     h_pool2_flat = tf.reshape(h_pool2, [-1, FINAL_VECTOR_SIZE])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
     # Final Layer
-    W_fc2 = weight_variable([FINAL_FEATURES, N_DIRECTIONS])
-    b_fc2 = bias_variable([N_DIRECTIONS])
+    W_fc2 = weight_variable([FINAL_FEATURES, N_DIRECTIONS], name = "W_fc2")
+    b_fc2 = bias_variable([N_DIRECTIONS], name = "b_fc2")
     # Output Layer
     output_layer = tf.nn.softmax(tf.matmul(h_fc1, W_fc2) + b_fc2)
     return input_layer, h_fc1, output_layer
@@ -74,6 +74,7 @@ class ConvNet:
     FUTURE_REWARD_DISCOUNT = 0.99
 
     def __init__(self):
+        self.step_counter = 0
         self._action = tf.placeholder("float", [None, N_DIRECTIONS])
         self._target = tf.placeholder("float", [None])
 
@@ -86,6 +87,7 @@ class ConvNet:
         self._train_operation = tf.train.AdamOptimizer(1e-6).minimize(cost)
 
         self._session.run(tf.initialize_all_variables())
+        self.saver = tf.train.Saver()
 
     def _train(self):
         pass
@@ -129,6 +131,8 @@ class ConvNet:
                 session=self._session)
 
             print('Trained')
+            self.saver.save(self._session, "convNet/model.ckpt")
+            self.step_counter += 1
         # L = pow(r + GAMMA*Q_table.max() + Qtable.max())/2
         # error_vector = numpy.zeros(N_DIRECTIONS)
         # error_vector[actionIndex] = L
